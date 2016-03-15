40d77bcf03c3e86da448bd713c122a437e60307d
Bean900 <n.nikisch@gmx.de>
2016-03-15 14:53:28 +0100

    GUI ist fertig

--- src/main/java/de/mosbach/lan/smarthomeClient/SmartHomeClient.java
+++ src/main/java/de/mosbach/lan/smarthomeClient/SmartHomeClient.java
@@ -5,24 +5,51 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
- 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 @SuppressWarnings("restriction")
 public class SmartHomeClient extends Application {
-   
-     
+
+	@FXML
+	private TextField internalTemperature;
+
+	@FXML
+	private TextField outsideTemperature;
+
+	@FXML
+	private TextField stateWindow;
+
+	@FXML
+	private TextField stateHeater;
+
+	@FXML
+	private TextField stateAirConditioner;
+
+	@FXML
+	private TextField insideTempRequirement;
+
 	public static void main(String[] args) {
-        launch(args);
-    }
+		launch(args);
+	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-	    Parent root = FXMLLoader.load(this.getClass().getResource("gui.fxml"));
-	    
-        Scene scene = new Scene(root, 300, 275);
-    
-        primaryStage.setTitle("Smarthome");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+		Parent root = FXMLLoader.load(this.getClass().getResource("gui.fxml"));
+
+		Scene scene = new Scene(root, 600, 425);
+
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+
+
+	}
+
+	@FXML
+	private void refresh() {
+		 
+		
 		
 	}
 }
